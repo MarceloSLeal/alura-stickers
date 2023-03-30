3fd9d57ce09da82f6e687cf532240bfaee4e8619
MarceloSLeal <marcelosleal@outlook.com>
2023-03-30 00:31:10 -0300

    Adicionando anotações de Jackson para reconhecer diferenças entre a classe e a propriedade no Json

--- src/main/java/Filmes.java
+++ src/main/java/Filmes.java
@@ -1,4 +1,6 @@
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,7 +12,11 @@ public class Filmes {
 
     private Integer id;
     private String title;
+
+    @JsonSetter("vote_average")
     private Double vote;
+
+    @JsonSetter("poster_path")
     private String path;
 
 }
