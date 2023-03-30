981848eaacab49670d50f48979fd2a3cc57da48d
MarceloSLeal <marcelosleal@outlook.com>
2023-03-30 00:34:00 -0300

    Classe criada apenas para fazer testes, o ideal seria usar Junit para esses testes unitários

--- src/main/java/Testes.java
+++ src/main/java/Testes.java
@@ -1,13 +1,11 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
 import java.io.IOException;
 
 public class Testes {
 
     public static void main(String[] args) throws IOException {
-
-        GeradorFigurinhas geradorFigurinhas =  new GeradorFigurinhas();
-
-//        geradorFigurinhas.criar("Urso doido");
-
+        
     }
-
 }
