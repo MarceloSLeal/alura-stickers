c5accab1890ea346537c366a4a3295d01839efad
MarceloSLeal <marcelosleal@outlook.com>
2023-03-30 00:29:18 -0300

    Reestruturando o main e dividindo o código para novas classes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,39 +11,22 @@ public class App {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-
         EndPointsMovies endPointsMovies = new EndPointsMovies();
         String corpoJson = endPointsMovies.topRated();
 
         ObjectMapper objectMapper = new ObjectMapper();
 
-
         JsonNode resultsNode = objectMapper.readTree(corpoJson).get("results");
 
         List<Filmes> filmes = new ArrayList<>();
 
-        GeradorFigurinhas geradorFigurinhas = new GeradorFigurinhas();
-        for (JsonNode movieNode : resultsNode) {
-            Integer id = movieNode.get("id").asInt();
-            String title = movieNode.get("title").asText();
-            Double vote = movieNode.get("vote_average").asDouble();
-            String path = movieNode.get("backdrop_path").asText();
+        filmes = CriarListaFilmes.criarLista(corpoJson);
 
-            filmes.add(new Filmes(id, title, vote, path));
-
-            String urlImagem = "https://image.tmdb.org/t/p/w500" + path;
-            InputStream inputStream = new URL(urlImagem).openStream();
-            String pathSaida = "src/main/resources/imagens/saida/" + title + ".png";
-
-            geradorFigurinhas.criar(inputStream, title, pathSaida);
-
-            System.out.println(title);
-
+        for (Filmes film : filmes) {
+            System.out.println(film.getId() + " : " + film.getTitle() + " : " + film.getVote() + " : " + film.getPath());
         }
 
-//        for (Filmes film : filmes) {
-//            System.out.println(film.getId() + " : " + film.getTitle() + " : " + film.getVote() + " : " + film.getPath());
-//        }
+        ChamarGeradorFigurinhas.chamar(filmes);
 
     }
 }
