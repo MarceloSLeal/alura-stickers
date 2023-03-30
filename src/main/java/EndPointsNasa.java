efd6af543a447f3e4fcd5ce702b90662f86e7577
MarceloSLeal <marcelosleal@outlook.com>
2023-03-30 03:10:04 -0300

    Criando a chamada do endpoint das imagens da Nasa

---
+++ src/main/java/EndPointsNasa.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Date;
+
+public class EndPointsNasa {
+
+    public String montarRequest(String start, String end) throws IOException, InterruptedException {
+
+        String url = "https://api.nasa.gov/planetary/apod?api_key=" + PegaChave.chaveNasa() +
+                "&start_date=" + start.toString() + "&end_date=" + end.toString();
+        URI endereco = URI.create(url);
+        HttpClient cliente = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = cliente.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
+
+}
