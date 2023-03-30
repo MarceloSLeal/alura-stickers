98697044f218fa4e986d66dabf324ac96d67628c
MarceloSLeal <marcelosleal@outlook.com>
2023-03-30 03:10:44 -0300

    Criando o objeto Nasa para receber os dados do Json

---
+++ src/main/java/Nasa.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Nasa {
+
+    private String title;
+    private String url;
+
+    public Nasa() {
+    }
+
+}
